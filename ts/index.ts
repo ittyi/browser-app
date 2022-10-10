752b715d058cd587d6930ba0a6adc1713b88c6a1
ittyi <vioiv.k4@gmail.com>
2022-10-11 00:24:44 +0900

    ✨ タスクのリスト化

--- ts/index.ts
+++ ts/index.ts
@@ -1,8 +1,10 @@
 import { EventListener } from './EventListener'
 import { Task } from "./Task";
+import { TaskCollection } from "./TaskCollection";
 
 class Application {
 	private readonly eventListener = new EventListener()
+	private readonly taskCollection = new TaskCollection()
 
 	start() {
 		const createForm = document.getElementById('createForm') as HTMLElement
@@ -20,6 +22,8 @@ class Application {
 
 		const task = new Task({ title: titleInput.value})
 		console.log(task)
+		this.taskCollection.add(task)
+		console.log(this.taskCollection)
 	}
 }
 
